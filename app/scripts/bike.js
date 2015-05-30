488de985a37e1a8f73a6fec9df5b45d6027e77c3
Finbar Sheahan <finbar.sheahan@gmail.com>
2015-05-30 11:37:50 +0100

    refactor bike to own class

---
+++ app/scripts/bike.js
@@ -0,0 +1,68 @@
+var d3 = require('d3-browserify');
+
+function Bike(image_file_name, container_svg, follow_path) {
+  var self = this;
+  var svg_file_path_base = process.env.NODE_ENV === 'development' ? '../images/' : '../cycle-counter/images/';
+  var svg_file_path = svg_file_path_base + image_file_name;
+  this.loadBike(svg_file_path, function(xml){
+    self.animateBikeOnPath(xml, container_svg, follow_path)
+  });
+};
+
+Bike.prototype.loadBike = function(svg_file_path, callback) {
+  var self = this;
+  d3.xml(svg_file_path, 'image/svg+xml', function(error, bikeSvg) {
+    callback.apply(self, [bikeSvg]);
+  });
+};
+
+Bike.prototype.animateBikeOnPath = function(xml, container_svg, follow_path) {
+    var self = this;
+    var importedNode = document.importNode(xml.documentElement, true);
+    var node = process.env.NODE_ENV === 'development' ? 1 : 0;
+    var path = importedNode.childNodes[node];
+
+    var startPoint = self.pathStartPoint(follow_path);
+    var pos_x = 0;
+    var pos_y = startPoint[1].substring(0, startPoint[1].indexOf('.')) - 39;
+    var marker = container_svg.append('path')
+      .attr('d', d3.select(path).attr('d'))
+      .attr('class', 'bike')
+      .attr('transform', 'translate(0,' + pos_y + ')')
+    self.transition(marker, follow_path);
+};
+
+Bike.prototype.pathStartPoint = function(path) {
+  var d = path.attr('d'),
+  dsplitted = d.split(' ');
+  return dsplitted[0].split(',');
+};
+
+Bike.prototype.transition = function(marker, follow_path) {
+  var self = this;
+  marker.transition()
+    .duration(14000)
+    .attrTween('transform', self.translateAlong(follow_path.node()))
+    .each('end', function () {
+      self.transition(marker, follow_path);
+    }); // infinite loop
+};
+
+Bike.prototype.translateAlong = function(path) {
+  var l = path.getTotalLength();
+  var t0 = 0;
+  return function(i) {
+    return function(t) {
+      var p0 = path.getPointAtLength(t0 * l);//previous point
+      var p = path.getPointAtLength(t * l);////current point
+      var angle = Math.atan2(p.y - p0.y, p.x - p0.x) * 180 / Math.PI;//angle for tangent
+      t0 = t;
+
+      var centerX = p.x - 32,
+      centerY = p.y - 40;
+      return 'translate(' + centerX + ',' + centerY + ')rotate(' + angle + ' 32' + ' 40' +')';
+    }
+  }
+};
+
+module.exports = Bike;
--- app/scripts/chart.js
+++ app/scripts/chart.js
@@ -1,6 +1,7 @@
 'use strict';
 var d3 = require('d3-browserify');
 var textures = require('textures/textures');
+var Bike = require('./bike');
 
 function Chart(container, model){
   this.container = container;
@@ -53,7 +54,7 @@ Chart.prototype.drawGraph = function () {
   var events = this.setEvents( {target: line1} );
   var axes = this.drawAxes();
   var self = this;
-  var animated_bike = this.addAnimatedBike(line1);
+  var animated_bike = new Bike('bike.svg', this.svg, line1);
 
   return {
     line1: line1,
@@ -64,61 +65,6 @@ Chart.prototype.drawGraph = function () {
   };
 };
 
-Chart.prototype.loadBike = function (callback) {
-  var self = this;
-  var svg_path = process.env.NODE_ENV === 'development' ? '../images/bike.svg' : '../cycle-counter/images/bike.svg';
-  d3.xml(svg_path, 'image/svg+xml', function(error, bikeSvg) {
-    callback.apply(self, [bikeSvg]);
-  });
-}
-
-Chart.prototype.addAnimatedBike = function(followPath) {
-    var self = this;
-    this.loadBike( function(svg){
-      var importedNode = document.importNode(svg.documentElement, true);
-      var node = process.env.NODE_ENV === 'development' ? 1 : 0;
-      var path = importedNode.childNodes[node];
-
-      function pathStartPoint(path) {
-        var d = path.attr("d"),
-        dsplitted = d.split(" ");
-        return dsplitted[0].split(",");
-      };
-
-      var startPoint = pathStartPoint(followPath);
-      var pos_x = 0;
-      var pos_y = startPoint[1].substring(0, startPoint[1].indexOf('.')) - 39;
-      var marker = self.svg.append('path')
-              .attr('d', d3.select(path).attr('d'))
-              .attr('class', 'bike')
-              .attr("transform", "translate(0," + pos_y + ")")
-      transition();
-
-      function transition() {
-        marker.transition()
-            .duration(17000)
-            .attrTween("transform", translateAlong(followPath.node()))
-            .each("end", transition); // infinite loop
-      };
-
-      function translateAlong(path) {
-        var l = path.getTotalLength();
-        var t0 = 0;
-        return function(i) {
-          return function(t) {
-            var p0 = path.getPointAtLength(t0 * l);//previous point
-            var p = path.getPointAtLength(t * l);////current point
-            var angle = Math.atan2(p.y - p0.y, p.x - p0.x) * 180 / Math.PI;//angle for tangent
-            t0 = t;
-
-            var centerX = p.x - 32,
-            centerY = p.y - 40;
-            return "translate(" + centerX + "," + centerY + ")rotate(" + angle + " 32" + " 40" +")";
-          }
-        }
-      }
-  });
-};
 Chart.prototype.createLine = function (field) {
   var self = this;
   var line = d3
