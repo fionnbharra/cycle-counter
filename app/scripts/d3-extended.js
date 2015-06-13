aab2a988840b0284764660e86e0d84bbf471dc1f
Finbar Sheahan <finbar.sheahan@gmail.com>
2015-06-13 09:24:24 +0100

    oops, no need for that

--- app/scripts/d3-extended.js
+++ app/scripts/d3-extended.js
@@ -1,15 +1,8 @@
 var d3 = require('d3-browserify');
-
-function D3extended (argument) {
-  this = d3;
-
-}
-
-D3extended.selection.prototype.moveToFront = function() {
+d3.selection.prototype.moveToFront = function() {
   return this.each(function(){
   this.parentNode.appendChild(this);
   });
 };
 
-
-module.exports = D3extended;
+module.exports = d3;
