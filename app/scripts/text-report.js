05a97bbc12fd62e5bd07ecac55e7880ee031b88f
Finbar Sheahan <finbar.sheahan@gmail.com>
2015-07-01 12:57:30 +0100

    adding text report

--- app/scripts/text-report.js
+++ app/scripts/text-report.js
@@ -1,12 +1,26 @@
 'use strict';
 var d3 = require('d3-browserify');
 
-function TextReport(model, container) {
-  this.writeReport(model, container);
+function TextReport(container, model) {
+  var yesterdaysData = this.parseData(model);
+  this.writeReport(container, yesterdaysData);
 }
 
-TextReport.prototype.writeReport = function(model, container) {
-  d3.select('html').classed("loading", false);
+TextReport.prototype.parseData = function(model) {
+  var dataset = model.dataset;
+  var lastItemIndex = dataset.length - 1;
+
+  return {
+    numberOfBikes: dataset[lastItemIndex].totalBikes,
+    temperature: dataset[lastItemIndex].temperatureMax
+  }
+};
+
+TextReport.prototype.writeReport = function(container, yesterdaysData) {
+    var numberContainer = d3.select('#number-of-bikes', container);
+    var temperatureContainer = d3.select('#temperature', container);
+    numberContainer.html(yesterdaysData.numberOfBikes);
+    temperatureContainer.html(yesterdaysData.temperature);
 };
 
 module.exports = TextReport;
